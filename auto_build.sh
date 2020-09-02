6cb550aecdeff9e8a942cd024a4fc39ea57d9238
Wxperia <914827352@qq.com>
2020-09-02 23:18:54 +0800

    独立自动部署脚本

--- auto_build.sh
+++ auto_build.sh
@@ -1,13 +1,13 @@
-#! /bin/bash
+# #! /bin/bash
 
-SITE_PATH='/usr/local/music-webapp'
-USER='wxperia'
-USERGROUP='admin'
+# SITE_PATH='/usr/local/music-webapp'
+# USER='wxperia'
+# USERGROUP='admin'
 
-cd $SITE_PATH
-git reset --hard origin/master
-git clean -f
-git pull
-git checkout master
-cnpm run build
-chown -R $USER:$USERGROUP $SITE_PATH
+# cd $SITE_PATH
+# git reset --hard origin/master
+# git clean -f
+# git pull
+# git checkout master
+# cnpm run build
+# chown -R $USER:$USERGROUP $SITE_PATH
